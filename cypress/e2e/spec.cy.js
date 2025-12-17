be906c49e97b3f8bbcf835fd866e4b2b4bb358fc
Jarkko Remes <ag1534@student.jamk.fi>
2025-12-17 21:42:14 +0200

    Initial commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress" />
+
+// describe - Mocha.js:n toiminto, joka kuvastaa testijoukkoa
+describe('My First Test', () => {
+  // it - Mocha.js:n toiminto, joka kuvastaa yksittäistä testitapausta
+  it('clicking "type" shows the right headings', () => {
+    // cy. - viittaa Cypressin toimintoihin
+    // visit - vireaillaa annetulla verkkosivulla
+    cy.visit('https://example.cypress.io');
+
+    // wait - odotetaan annetun millisekuntien ajan.
+    cy.wait(5000);
+
+    // contains - etsii DOM-elementin joka sisältää annetun arvon
+    // click - klikataan DOM-elementtiä
+    cy.contains('type').click();
+
+    cy.wait(5000);
+
+    // url - haetaan tämänhetkinen URL
+    // should - jokin tulisi olla jotain, määritellään argumenteillä
+    // include - sisältää jonkin annetun arvon
+    cy.url().should('include', '/commands/actions');
+
+    // get - haetaan DOM-elementti
+    // type - kirjoitetaan DOM-elementtiin
+    // have.value - tarkistetaan, että valitussa elementissä on annettu arvo
+    // piste-operaattorilla voidaan ketjuttaa komentoja
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com');
+  });
+});
